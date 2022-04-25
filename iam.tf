b6445f8c4e68be15b003e03234302b0951828dc4
Sreelal <sreelalr@gmail.com>
2022-04-25 07:52:55 +0530

    fixed version

--- iam.tf
+++ iam.tf
@@ -1,49 +1,49 @@
-# Instance profile
+#Creat IAM Role for ECR
+resource "aws_iam_role" "ecr_role" {
+  name               = "ecr-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": ["ec2.amazonaws.com"]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+#Create IAM policy to allow access to ECR
+resource "aws_iam_policy" "ecr_policy" {
+  name = "ecr-access-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+# Attach policy to the role
+resource "aws_iam_policy_attachment" "attach" {
+  name       = "ecr-attach"
+  roles      = ["${aws_iam_role.ecr_role.name}"]
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+#Instance profile
 resource "aws_iam_instance_profile" "iam_ec2_profile" {
   name = "iam-instance-profile"
   role = aws_iam_role.ecr_role.name
 }
-
-# # Creat IAM Role for ECR
-# resource "aws_iam_role" "ecr_role" {
-#   name               = "ecr-role"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": ["ec2.amazonaws.com"]
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-
-# }
-
-# #Create IAM policy to allow access to ECR
-# resource "aws_iam_policy" "ecr_policy" {
-#   name = "ecr-access-policy"
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "ecr:*",
-#         ]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
-
-# # Attach policy to the role
-# resource "aws_iam_policy_attachment" "attach" {
-#   name       = "ecr-attach"
-#   roles      = ["${aws_iam_role.ecr_role.name}"]
-#   policy_arn = aws_iam_policy.ecr_policy.arn
-# }
--- vpc.tf
+++ vpc.tf
@@ -70,7 +70,7 @@ resource "aws_route_table" "web-rt" {
 
 # Create Web Subnet association with Web route table
 resource "aws_route_table_association" "rt_association" {
-  count          = var.item_coaws_iam_instance_profileunt
+  count          = var.item_count
   subnet_id      = aws_subnet.web-subnet[count.index].id
   route_table_id = aws_route_table.web-rt.id
 }
